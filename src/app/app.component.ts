5ad642aaf05922c945df0b39992212f05cc9d266
Edar Stojku <kuroki2201@outlook.com>
2023-10-02 16:56:44 +0200

    update new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import * as AOS from 'aos';
 })
 export class AppComponent implements OnInit {
   ngOnInit(): void {
-    AOS.init({disable: 'mobile'});
+    AOS.init();
     AOS.refresh();
   }
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -8,12 +8,25 @@ import { Component } from '@angular/core';
 export class ProjectsComponent {
 
   public images: string[] = [
-    'assets/images/portfolio/fullsize/1.jpg',
-    'assets/images/portfolio/fullsize/2.jpg',
-    'assets/images/portfolio/fullsize/3.jpg',
-    'assets/images/portfolio/fullsize/4.jpg',
-    'assets/images/portfolio/fullsize/5.jpg',
-    'assets/images/portfolio/fullsize/6.jpg',
+    'assets/images/portfolio/fullsize/1400x919-MargaritaPizza-555a4065-2573-4b41-bcf3-7193cd095d8f-0-1400x919.jpg',
+    'assets/images/portfolio/fullsize/HD-wallpaper-fitness-girl-gym-girl-black-fitness-woman.jpg',
+    'assets/images/portfolio/fullsize/gettyimages-933898972-640x640.jpg',
+    'assets/images/portfolio/fullsize/360_F_610620762_tn5NVwSRKvUV5UXibzopJeWuIyS44Dsd.jpg',
+  ];
+
+   imageInfo: any = [
+    { category: 'Web application', name: 'Pizza Food Delievery' },
+    { category: 'Web application', name: 'Gym Registration Plaform' },
+    { category: 'Application', name: 'Finger Tracker' },
+    { category: 'Software', name: 'Language Identification Tool' },
+    
+  ];
+
+  public imageLinks: string[] = [
+    'https://github.com/martgjepali/PizzaDelivery',
+    'https://github.com/martgjepali/gym_registration_app',
+    'https://github.com/Fori99/Finger-Tracker',
+    'https://github.com/Fori99/Language-Identification-Tool',
   ];
 
   public currentImageIndex: number = 0;
