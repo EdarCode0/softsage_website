ce8c03f1bf407cbedf3f346b8fd598a1a3e6f4d3
Edar Stojku <kuroki2201@outlook.com>
2023-10-27 00:35:13 +0200

    update

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -8,25 +8,25 @@ import { Component } from '@angular/core';
 export class ProjectsComponent {
 
   public images: string[] = [
-    'assets/images/portfolio/fullsize/1400x919-MargaritaPizza-555a4065-2573-4b41-bcf3-7193cd095d8f-0-1400x919.jpg',
     'assets/images/portfolio/fullsize/HD-wallpaper-fitness-girl-gym-girl-black-fitness-woman.jpg',
     'assets/images/portfolio/fullsize/gettyimages-933898972-640x640.jpg',
     'assets/images/portfolio/fullsize/360_F_610620762_tn5NVwSRKvUV5UXibzopJeWuIyS44Dsd.jpg',
+    'assets/images/UNYT.png',
   ];
 
    imageInfo: any = [
-    { category: 'Web application', name: 'Pizza Food Delievery' },
-    { category: 'Web application', name: 'Gym Registration Plaform' },
-    { category: 'Application', name: 'Finger Tracker' },
-    { category: 'Software', name: 'Language Identification Tool' },
+     { category: 'Web application', name: 'Gym Registration Plaform' },
+     { category: 'Application', name: 'Finger Tracker' },
+     { category: 'Software', name: 'Language Identification Tool' },
+     { category: 'Web application', name: 'Data Analysis Application' },
     
   ];
 
   public imageLinks: string[] = [
-    'https://github.com/martgjepali/PizzaDelivery',
     'https://github.com/martgjepali/gym_registration_app',
     'https://github.com/Fori99/Finger-Tracker',
     'https://github.com/Fori99/Language-Identification-Tool',
+    'https://github.com/Parisxhetani/DataAnalysis',
   ];
 
   public currentImageIndex: number = 0;
